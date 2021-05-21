b218c364cf5d5bac459e68b11b602c422918180f
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2021-05-21 16:52:17 -0300

    Add project files.

---
+++ NovoApp/NovoApp/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NovoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public int Score { get; set; }
+        public int Time { get; set; }
+
+        public List<Words> WordsList = new List<Words>()
+        {
+            new Words("int"),
+            new Words("class"),
+            new Words("double")
+        };
+
+
+        public MainPage()
+        {
+            Time = 5 * 60;
+            InitializeComponent();
+            WordCollection.ItemsSource = WordsList;
+            UpdateScoreLabel();
+            Task.Run(async () =>
+            {
+                while (Score < WordsList.Count &&
+                      Time > 0)
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        TimerLabel.Text = $"TIMER {Time} SECONDS";
+                    });
+                       await Task.Delay(1000);
+                    Time = Time - 1;
+                }                
+            });
+        }
+
+        private void IncrementScore() => Score++;
+
+        private void UpdateScoreLabel() => ScoreLabel.Text = $"SCORE {Score}/{WordsList.Count}";
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var word = WordEntry.Text;
+            WordEntry.Text = "";
+
+            var wordFound = WordsList.FirstOrDefault(p => p.Name == word);
+            if (wordFound != null) {
+                wordFound.Found = true;
+                IncrementScore();
+                UpdateScoreLabel();
+            }
+        }
+    }
+}
---
+++ NovoApp/NovoApp/Words.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NovoApp
+{
+    public class Words
+    {
+        public bool Found { get; set; }
+        public string Name { get; set; }
+
+        public Words(string name) => Name = name;
+
+    }
+}
