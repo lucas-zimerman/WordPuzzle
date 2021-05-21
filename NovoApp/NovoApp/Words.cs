b5eee6a64d52e4710572bc5d07f84846f22d2ef5
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2021-05-21 17:22:43 -0300

    -Fix Layout bindings and added a minimun height for each item. -Words Found now invokes the property changed.,

--- NovoApp/NovoApp/Words.cs
+++ NovoApp/NovoApp/Words.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace NovoApp
 {
-    public class Words
+    public class Words : INotifyPropertyChanged
     {
-        public bool Found { get; set; }
+        private bool _found;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler == null) return;
+            handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool Found 
+        {
+            get => _found;
+            set
+            {
+                _found = value;
+                RaisePropertyChanged();
+            } 
+        }
         public string Name { get; set; }
 
         public Words(string name) => Name = name;
